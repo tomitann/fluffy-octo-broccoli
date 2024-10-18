d92cbfcab0db03d9ae1fa3e016ecf2c3324c385f
tomitann <t236108@st.yamagata-u.ac.jp>
2024-10-18 19:25:27 +0900

    Add files via upload

---
+++ 課題３.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int k;
+    printf("How many sheeps?");
+    scanf("%d",&k);
+    
+    int b;
+    printf("How many ohashi?");
+    scanf("%d",&b);
+    
+    printf("The number of animai is %d", k+b);
+    
+    return 0;
+}
+}
