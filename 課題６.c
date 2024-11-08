ed56d01f558fce134f04a58e8ae3f2bd9e5028c0
tomitann <t236108@st.yamagata-u.ac.jp>
2024-11-08 19:42:47 +0900

    Add files via upload

--- 課題６.c
+++ 課題６.c
@@ -5,10 +5,18 @@ int main()
     int k;
     printf("How much did you contribute to the favorite character? ");
     scanf("%d",&k);
-    
-    if (k>=50000)
+    int n;
+    printf("How much did you contribute to the favorite pet? ");
+    scanf("%d",&n);
+   
+    if ((k+n)>=50000)
     {
         printf("I spent a lot of money");
+    }
+    
+    else if ((k+n)>=1000 && (k+n)<50000)
+    {
+        printf("I spent some money");
     }
     
     else
